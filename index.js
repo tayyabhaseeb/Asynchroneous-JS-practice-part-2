5398679746d3f66396bcc977a3bb27dfac961402
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-08-13 23:13:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => console.log(data))
